4424277a679ef7dad73d9a6df6f359f04d781c7a
Josiah Schmidt <37901964+jnschmidt18@users.noreply.github.com>
2021-10-04 15:52:53 -0400

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -4,11 +4,15 @@
 import ctypes
 import time
 
-for i in range (5000):                              #loop 5000 times, roughly 20s
-    ctypes.windll.user32.SetCursorPos(275,400)      #move mouse to 275,400 position
-    ctypes.windll.user32.mouse_event(2,0,0,0,0)     #mouse left click down
-    ctypes.windll.user32.mouse_event(4,0,0,0,0)     #mouse left click up
-    time.sleep (0.00001)                            #prevents the system from crashing, don't remove this
-    
-#250 cycles per second
-#15,000 cycles per minute
+def click():
+    for i in range (5000):                              #loop 5000 times, roughly 20s
+        ctypes.windll.user32.SetCursorPos(275,400)      #move mouse to 275,400 position
+        ctypes.windll.user32.mouse_event(2,0,0,0,0)     #mouse left click down
+        ctypes.windll.user32.mouse_event(4,0,0,0,0)     #mouse left click up
+        time.sleep (0.00001)                            #prevents the system from crashing, don't remove this
+
+
+go=str(input("type start to start: "))
+while(go=="start"):
+    click()
+    go=str(input("go again? 'start' to go: "))
